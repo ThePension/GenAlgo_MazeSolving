289087caccf224faaddd0ecd302ae8def4951281
Aubert Nicolas <nicolas.aubert@he-arc.ch>
2022-11-25 15:05:14 +0100

    Updating report

--- parallel_tests.py
+++ parallel_tests.py
@@ -1,10 +1,12 @@
 import multiprocessing as mp
 from labyrinth_with_deap import solve_labyrinth
 import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    file = "grid10.npy"
+    file = "./test_grids/realisticGrid30.npy"
     grid = np.load(file)
+    grid[1][1] = 0
     start = (0, 0)
     h = grid.shape[0]
     w = grid.shape[1]
@@ -12,12 +14,21 @@ if __name__ == "__main__":
 
     pool = mp.Pool(mp.cpu_count())
 
-    print(mp.cpu_count())
-
-    results = pool.starmap(solve_labyrinth, [(grid, start, end, 3) for i in range(100)])
+    results = pool.starmap(solve_labyrinth, [(grid, start, end, 180) for i in range(100)])
 
     lengths = [len(path) for path in results]
 
     pool.close()
+    
+    # Set the inverval for the bins to 1
+    plt.xticks(np.arange(min(lengths), max(lengths)+1, 1.0))
         
-    print(lengths)
+    plt.hist(lengths)
+    
+    # Add labels for length of path
+    plt.xlabel("Length of path")
+    
+    # Add labels for number of paths
+    plt.ylabel("Number of paths")
+    
+    plt.show()
--- test.py
+++ test.py
@@ -2,7 +2,18 @@ from labyrinth_with_deap import compute_subpath, random_individual
 from labyrinth_with_deap import creator
 import numpy as np
 
+from mazegenerator import MazeGenerator
+
 import time
+
+def random_maze(w : int, h : int, wall_ratio : float) -> np.ndarray:
+    maze = np.zeros((h, w), dtype=int)
+    for i in range(0, h):
+        for j in range(0, w):
+            if np.random.rand() < wall_ratio:
+                maze[i, j] = 1
+    return maze
+
 
 if __name__ == "__main__":
     # file = "grid10.npy"
@@ -13,14 +24,19 @@ if __name__ == "__main__":
     # path = compute_subpath(ind, (0, 0), grid, (9, 9))
     # print(path)
     
-    ind = random_individual(int(30 ** 2 / 2))
+    # ind = random_individual(int(30 ** 2 / 2))
     
-    file = "realGrid30.npy"
-    grid = np.load(file)
-    start = (1, 1)
-    h = grid.shape[0]
-    w = grid.shape[1]
-    end = (h - 2, w - 2)
+    # file = "realGrid30.npy"
+    # grid = np.load(file)
+    # start = (1, 1)
+    # h = grid.shape[0]
+    # w = grid.shape[1]
+    # end = (h - 2, w - 2)
         
-    best_path = compute_subpath(ind, start, grid, end)
+    # best_path = compute_subpath(ind, start, grid, end)
+    
+    dim_x, dim_y = 30, 30
+    wall_ratio = 0.3
+    
+    np.save("realisticGrid30.npy", MazeGenerator.generate_maze(dim_x, dim_y))
     
